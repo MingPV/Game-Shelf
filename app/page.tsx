966684115219d4520f8cf514032d6dcf3f2e6162
MingPV <jeung.pavee@gmail.com>
2025-01-23 16:10:06 +0700

    install daisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ export default async function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <div className="flex items-center justify-center font-bold">
-          This is Home Page
+        <div className="flex flex-col items-center justify-center font-bold">
+          <div>This is Home Page</div>
+          <div>button from daisyUI</div>
+          <button className="btn bg-base-300">Click me</button>
         </div>
       </main>
     </>
