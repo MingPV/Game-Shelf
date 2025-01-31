d5cb163235ed52b9240a5d084abd3af2e723ec53
Aunyapat Nitijarasrat <aunyapat2003@gmail.com>
2025-01-31 16:14:25 +0700

    create timeline component and interface

--- app/(user-pages)/profile/page.tsx
+++ app/(user-pages)/profile/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { StatusTracking } from "../../../components/inventory/status-tracking"; // Ensure the path is correct
+import { RentingRequest } from "@/app/types/game"; // Ensure the type is correct
 
 export default async function Home() {
   const supabase = await createClient();
@@ -11,11 +13,23 @@ export default async function Home() {
     redirect("/sign-in");
   }
 
+  const rentingRequest: RentingRequest = {
+    id: 1,
+    start_date: "2021-07-01",
+    end_date: "2021-07-08",
+    status: "pending",
+    customer_id: "1",
+    bg_id: "1",
+    created_at: "2021-07-01",
+  };
+
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <div className="flex flex-col items-center justify-center font-bold">
           <div>user profile page</div>
+          <StatusTracking rentingRequest={rentingRequest} />{" "}
+          {/* Ensure the prop is passed correctly */}
         </div>
       </main>
     </>
--- app/types/game.ts
+++ app/types/game.ts
@@ -1,1 +1,20 @@
 // game interface
+export interface Boardgame {
+  id: number;
+  provier_id: number;
+  bg_name: string;
+  description: string;
+  bg_picture: string;
+  price: number;
+  created_at: string;
+}
+
+export interface RentingRequest {
+  id: number;
+  start_date: string;
+  end_date: string;
+  status: string;
+  customer_id: string;
+  bg_id: string;
+  created_at: string;
+}
