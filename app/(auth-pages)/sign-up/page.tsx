91a64e90c0d2020341f32fdfa77d73164935ea1d
MingPV <jeung.pavee@gmail.com>
2025-02-07 20:43:55 +0700

    fix sign-up height when has error message.

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -1,20 +1,25 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import { signUpAction, checkUsernameUnique, checkEmailUnique } from "../actions"; 
+import {
+  signUpAction,
+  checkUsernameUnique,
+  checkEmailUnique,
+} from "../actions";
 import Link from "next/link";
 import { FaRegTimesCircle, FaRegCheckCircle } from "react-icons/fa";
 
-
 export default function Signup() {
-  const [isProvider, setIsProvider] = useState<boolean|null>(null);
+  const [isProvider, setIsProvider] = useState<boolean | null>(null);
   const [error, setError] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isEmailUnique, setIsEmailUnique] = useState<boolean|null>(null);
-  const [isEmailValid, setIsEmailValid] = useState<boolean|null>(null);
-  const [isUsernameUnique, setIsUsernameUnique] = useState<boolean|null>(null);
+  const [isEmailUnique, setIsEmailUnique] = useState<boolean | null>(null);
+  const [isEmailValid, setIsEmailValid] = useState<boolean | null>(null);
+  const [isUsernameUnique, setIsUsernameUnique] = useState<boolean | null>(
+    null
+  );
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -54,30 +59,32 @@ export default function Signup() {
     setEmail(newEmail);
     const isUnique = await checkEmailUnique(newEmail);
     setIsEmailUnique(isUnique);
-    if(isUnique === false){
+    if (isUnique === false) {
       setError("This email is already used");
       return;
     }
-    if(!/\S+@\S+\.\S+/.test(newEmail)){
+    if (!/\S+@\S+\.\S+/.test(newEmail)) {
       setIsEmailValid(false);
       setError("Email is invalid");
-    }else{
+    } else {
       setIsEmailValid(true);
       setError("");
     }
   };
 
-  const handleUsernameChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUsernameChange = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setError("");
     const newUsername = e.target.value;
     setUsername(newUsername);
     const isUnique = await checkUsernameUnique(newUsername);
     setIsUsernameUnique(isUnique);
-    if(isEmailUnique === false){
+    if (isEmailUnique === false) {
       setError("This email is already used");
       return;
     }
-    if(isUnique === false){
+    if (isUnique === false) {
       setError("This username is already used");
     }
   };
@@ -88,15 +95,26 @@ export default function Signup() {
       <div className="flex flex-1 justify-center items-center  text-white p-8">
         <div className="text-center space-y-6">
           <img
-            src={isProvider === null ? "/mock_user.jpeg" : isProvider === true ? "/mock_provider.jpeg" : "/mock_player.jpg"} // Replace with your image path
+            src={
+              isProvider === null
+                ? "/mock_user.jpeg"
+                : isProvider === true
+                  ? "/mock_provider.jpeg"
+                  : "/mock_player.jpg"
+            } // Replace with your image path
             alt="Signup Illustration"
             className="max-w-md mx-auto"
           />
           <h1 className="text-4xl font-bold">
-            {isProvider === null ? "Welcome" : isProvider === true ? "Provider" : "Player"}
+            {isProvider === null
+              ? "Welcome"
+              : isProvider === true
+                ? "Provider"
+                : "Player"}
           </h1>
           <p className="text-lg">
-            Join us and become part of an amazing community. Explore endless possibilities as a player or provider.
+            Join us and become part of an amazing community. Explore endless
+            possibilities as a player or provider.
           </p>
         </div>
       </div>
@@ -109,13 +127,19 @@ export default function Signup() {
               <h1 className="text-2xl font-medium">Sign up</h1>
               <p className="text-sm text text-foreground">
                 Already have an account?{" "}
-                <Link className="text-gs_purple font-medium hover:text-gs_white" href="/sign-in">
+                <Link
+                  className="text-gs_purple font-medium hover:text-gs_white"
+                  href="/sign-in"
+                >
                   Sign in
                 </Link>
               </p>
             </div>
 
-            <form className="flex flex-col min-w-64 gap-7 placeholder:text-gs_white" onSubmit={handleSubmit}>
+            <form
+              className="flex flex-col min-w-64 gap-7 placeholder:text-gs_white"
+              onSubmit={handleSubmit}
+            >
               <div className="relative flex items-center gap-2">
                 <input
                   type="email"
@@ -124,17 +148,15 @@ export default function Signup() {
                   onChange={handleEmailChange}
                   className="flex-1 p-2 border-b-2 border-gray-300 bg-transparent focus:outline-none focus:border-gs_purple"
                 />
-                {(isEmailUnique === true && isEmailValid == true) && (
-                  <FaRegCheckCircle className="absolute right-2 text-gs_green"/> 
+                {isEmailUnique === true && isEmailValid == true && (
+                  <FaRegCheckCircle className="absolute right-2 text-gs_green" />
                 )}
                 {(isEmailUnique === false || isEmailValid == false) && (
-                  <FaRegTimesCircle className="absolute right-2 text-gs_red"/>
+                  <FaRegTimesCircle className="absolute right-2 text-gs_red" />
                 )}
-                {(isEmailUnique === null || isEmailValid == null) && (
-                  <></> 
-                )}
+                {(isEmailUnique === null || isEmailValid == null) && <></>}
               </div>
-              
+
               <div className="relative flex items-center gap-2">
                 <input
                   type="text"
@@ -144,14 +166,12 @@ export default function Signup() {
                   className="flex-1 p-2 border-b-2 border-gray-300 bg-transparent focus:outline-none focus:border-gs_purple"
                 />
                 {isUsernameUnique === true && (
-                  <FaRegCheckCircle className="absolute right-2 text-gs_green"/>
+                  <FaRegCheckCircle className="absolute right-2 text-gs_green" />
                 )}
                 {isUsernameUnique === false && (
-                  <FaRegTimesCircle className="absolute right-2 text-gs_red"/>
+                  <FaRegTimesCircle className="absolute right-2 text-gs_red" />
                 )}
-                {isUsernameUnique === null && (
-                  <></> 
-                )}
+                {isUsernameUnique === null && <></>}
               </div>
               <input
                 type="password"
@@ -163,28 +183,35 @@ export default function Signup() {
               <div className="flex flex-col gap-2 pt-2">
                 <label>Select Role</label>
                 <div className="flex gap-2">
-                  <button 
-                    type="button" 
+                  <button
+                    type="button"
                     className={`btn flex-1 hover:bg-gs_purple hover:border-none ${isProvider === false ? "bg-gs_purple_gradient border-none" : "bg-transparent border-white"}`}
-                    onClick={() => setIsProvider(false)}>
+                    onClick={() => setIsProvider(false)}
+                  >
                     Player
                   </button>
-                  <button 
-                    type="button" 
-                    className={`btn flex-1 hover:bg-gs_purple hover:border-none ${isProvider ===true ? "bg-gs_purple_gradient border-none" : "bg-transparent border-white"}`}
-                    onClick={() => setIsProvider(true)}>
+                  <button
+                    type="button"
+                    className={`btn flex-1 hover:bg-gs_purple hover:border-none ${isProvider === true ? "bg-gs_purple_gradient border-none" : "bg-transparent border-white"}`}
+                    onClick={() => setIsProvider(true)}
+                  >
                     Provider
                   </button>
                 </div>
               </div>
-              <button type="submit" className="btn bg-gs_black hover:bg-gs_black hover:bg-opacity-60 hover:border-none">
-                Create  Account
+              <button
+                type="submit"
+                className="btn bg-gs_black hover:bg-gs_black hover:bg-opacity-60 hover:border-none"
+              >
+                Create Account
               </button>
-              {error.length > 0 && (
-                <div className="text-red-500">
-                  <p>{error}</p>
-                </div>
-              )}
+              <div className="h-12">
+                {error.length > 0 && (
+                  <div className="text-red-500">
+                    <p>{error}</p>
+                  </div>
+                )}
+              </div>
             </form>
           </div>
         </div>
