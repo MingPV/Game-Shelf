1f18e15c0e513456d87bab957da25e9dbd304420
warboo <warissara.liw@gmail.com>
2025-02-08 14:00:44 +0700

    feat: change form ui, move button to client component, and auto redirect.

--- app/(user-pages)/provider-form/page.tsx
+++ app/(user-pages)/provider-form/page.tsx
@@ -7,43 +7,49 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { createClient } from "@/utils/supabase/client";
 import { updateProviderAction } from "../actions";
-import { addVerificationRequest } from "@/app/(admin-pages)/actions";
+import { addVerificationRequest, selectVerificationRequest } from "@/app/(admin-pages)/actions";
+import { useRouter } from "next/navigation";
 
-export default function Signup() {
-  // const [isProvider, setIsProvider] = useState<boolean | null>(null);
-  // const [error, setError] = useState("");
-  // const [phoneNumber, setPhoneNumber] = useState("");
-  // const [location, setLocation] = useState("");
-  // const [paymentMethod, setPaymentMethod] = useState("");
-  // const [credentials, setCredential] = useState("");
+export default function ProviderForm() {
 
-  // const [username, setUsername] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [isEmailUnique, setIsEmailUnique] = useState<boolean | null>(null);
-  // const [isUsernameUnique, setIsUsernameUnique] = useState<boolean | null>(
-  //   null
-  // );
-
-  const [userId, setUserId] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string>("");
   const [error, setError] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
-      async function fetchUserId() {
-          const supabase = createClient();
-          const { data: { user }, error } = await supabase.auth.getUser();
+    async function fetchUserId() {
+      const supabase = createClient();
+      const { data: { user }, error } = await supabase.auth.getUser();
 
-          if (error || !user) {
-              setError("Failed to fetch user data.");
-              return;
-          }
-          
-          setUserId(user.id);
+      if (error || !user) {
+        setError("Failed to fetch user data.");
+        return;
       }
 
-      fetchUserId();
+      setUserId(user.id);
+    }
+
+    fetchUserId();
   }, []);
 
+  useEffect(() => {
+    if (userId === "") return; 
+
+    async function fetchReqData() {
+      try {
+        const req = await selectVerificationRequest(userId);
+        if (req) {
+          console.log("uid:", userId)
+          console.log("req:", req);
+          router.push("/")
+        }
+      } catch (error) {
+        console.error("Error fetching verification request:", error);
+      }
+    }
+
+    fetchReqData();
+  }, [userId]); 
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -74,6 +80,7 @@ export default function Signup() {
 
                     <Label htmlFor="phone_number">Phone Number *</Label>
                     <Input 
+                        className="placeholder:text-gray-300"
                         name="phone_number" 
                         pattern="0\d-\d{3}-\d{4}|0\d{2}-\d{3}-\d{4}"
                         placeholder="0X-XXX-XXXX or 0XX-XXX-XXXX" 
@@ -81,20 +88,23 @@ export default function Signup() {
                     />
 
                     <Label htmlFor="location">Location *</Label>
-                    <Input name="location" placeholder="Location" required />
+                    <Input className="placeholder:text-gray-300" name="location" placeholder="Location" required />
 
                     <Label htmlFor="payment_method">Payment Method *</Label>
-                    <Input name="payment_method" placeholder="Payment Method" required />
+                    <Input className="placeholder:text-gray-300" name="payment_method" placeholder="Payment Method" required />
 
                     <Label htmlFor="credentials">Creadentials *</Label>
                     <input name="credentials" type="file" required />
                     
-                    <button
-                        type="submit"
-                        className="btn bg-gs_black hover:bg-gs_black hover:bg-opacity-60 hover:border-none"
-                    >
-                        Submit
-                    </button>
+                    <div className="flex justify-center">
+                        <button
+                            type="submit"
+                            className="btn rounded-full bg-gs_purple_gradient hover:bg-opacity-60 border-none w-min px-16"
+                        >
+                            Submit
+                        </button>
+                    </div>
+
                     {error.length > 0 && (
                     <div className="text-red-500">
                         <p>{error}</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Counter from "@/components/counter";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import Link from "next/link";
-import { selectVerificationRequest } from "./(admin-pages)/actions";
-
+import VerificationButton from "@/components/user-pages/verification-button";
 export default async function Home() {
   const supabase = await createClient();
 
@@ -27,14 +25,6 @@ export default async function Home() {
     } else {
       //  select index 0 of array [user_data] => user_data
       user_data_value = user_data[0];
-      if (user_data_value.isProvider) {
-        try {
-          const req = await selectVerificationRequest(user_data_value.uid);
-          if (req) sentVerifyReq = req.status;
-        }
-        catch { }
-      }
-
     }
   }
 
@@ -52,9 +42,7 @@ export default async function Home() {
             {user_data_value?.isProvider ? (
               <div className="flex flex-col items-center gap-2">
                 You are Provider 
-                { (sentVerifyReq === "pending") && <div className="border rounded-md p-2 text-info border-info" >Waiting For Verification</div> }
-                { (sentVerifyReq === "verified") && <div className="border rounded-md p-2 text-green-400 border-green-400" >Verification Completed!</div> }
-                { (sentVerifyReq === "") && <Link className="btn btn-outline btn-primary" href="/provider-form">Request For Verification</Link> }
+                <VerificationButton provider_id={user_data_value.uid} />
               </div>
             ) : (
               <div>You are Customer</div>
