fdcf78afc6bc1e6c140779321da48fdc3e281209
imacareforu <imacareforu@gmail.com>
2025-02-09 06:47:32 +0700

    bg-tracking-page v0.2

--- components/boardgame-tracking/bg-tracking-card.tsx
+++ components/boardgame-tracking/bg-tracking-card.tsx
@@ -1,10 +1,13 @@
+"use client"
+import { useState } from "react";
 export default function BoardGameCard({name, picture, price, description, status}:
     {name: string, picture: string, price: number, description: string, status: string}) {
 
     const statusClass = status === 'available' ? 'bg-green-500' : 'bg-orange-500';
+    const [showProgess, setShowProgess] = useState(false);
 
     return (
-        <div className="card card-side shadow-xl border-white border-[1px] m-5 h-[250px] relative">
+        <div className={`card card-side shadow-xl border-white border-[1px] m-5 relative transition-all duration-300 ${showProgess ? 'h-[350px]' : 'h-[250px]'}`}>
             <img
                 src={picture}
                 alt="boardgame picture" 
@@ -12,15 +15,27 @@ export default function BoardGameCard({name, picture, price, description, status
             />
             <div className="card-body flex flex-col">
                 <p className="text-2xl font-semibold">{name}</p>
-                <button className={`absolute top-0 right-0 mt-5 mr-10 rounded px-5 py-2 ${statusClass}`}>
+                <button className={`absolute top-0 right-0 mt-5 mr-10 rounded px-5 py-2 ${statusClass}`}
+                onClick={() => setShowProgess(!showProgess)}
+                >
                     {status}
                 </button>
                 <p>type: </p>
                 <p>price: {price}</p>
-                <p>{description}</p>
-                <div className="flex justify-end mt-auto">
-                    <button className="absolute bottom-0 right-0 mb-5 mr-10 bg-emerald-600 rounded px-5 py-2 hover:bg-emerald-700">edit condition or details</button>
+                <div>
+                    <p>{description}</p>
+                    <div className="flex justify-end">
+                        <button className="bg-emerald-600 rounded px-5 py-2 hover:bg-emerald-700">edit condition or details</button>
+                    </div>
                 </div>
+                {showProgess && (
+                    <ul className="steps mt-5">
+                        <li className="step step-primary">Register</li>
+                        <li className="step step-primary">Choose plan</li>
+                        <li className="step">Purchase</li>
+                        <li className="step">Receive Product</li>
+                    </ul>
+                )}
             </div>
         </div>
     );
