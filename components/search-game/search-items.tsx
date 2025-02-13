b3982ba6415280544ae7a849c78a746681ab47e9
Aunyapat Nitijarasrat <aunyapat2003@gmail.com>
2025-02-13 08:44:42 +0700

    change debounce

--- components/search-game/search-items.tsx
+++ components/search-game/search-items.tsx
@@ -57,7 +57,7 @@ export function SearchItems() {
     }
 
     setIsFetching(false);
-  }, 500);
+  }, 700);
 
   const getBoardgameType = async () => {
     const data = await selectAllBoardgameType();
