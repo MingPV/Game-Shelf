45bc2606fce7f231e0867319518105e3f9720ee8
MingPV <jeung.pavee@gmail.com>
2025-02-28 14:34:43 +0700

    change sign-in page to client and change all layout to client

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,35 +1,55 @@
+"use client";
+
 import { signOutAction } from "@/app/(auth-pages)/actions";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
-import { createClient } from "@/utils/supabase/server";
+import { useEffect, useState } from "react";
+import { UserData } from "@/app/types/user";
+import { getMyUserData } from "@/app/(user-pages)/actions";
 
-export default async function AuthButton() {
-  const supabase = await createClient();
+export default function AuthButton() {
+  // const supabase = await createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
 
-  let profile_url =
-    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
+  // let profile_url =
+  //   "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
 
-  const { data: user_data, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("uid", user?.id);
+  // const { data: user_data, error } = await supabase
+  //   .from("users")
+  //   .select("*")
+  //   .eq("uid", user?.id);
 
-  let isProvider = false;
+  // let isProvider = false;
 
-  if (user_data) {
-    if (user_data[0].profilePicture) {
-      profile_url = user_data[0].profilePicture;
-    }
-    if (user_data[0].isProvider) {
-      isProvider = true;
-    }
-  }
+  // if (user_data) {
+  //   if (user_data[0].profilePicture) {
+  //     profile_url = user_data[0].profilePicture;
+  //   }
+  //   if (user_data[0].isProvider) {
+  //     isProvider = true;
+  //   }
+  // }
+
+  const [myData, setMyData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchData = await getMyUserData();
+
+      setMyData(fetchData);
+    };
+    fetchData();
+  }, []);
+
+  const handleSignOut = async () => {
+    setMyData(null);
+    signOutAction();
+  };
 
   if (!hasEnvVars) {
     return (
@@ -67,7 +87,7 @@ export default async function AuthButton() {
       </>
     );
   }
-  return user ? (
+  return myData ? (
     <div className="flex items-center gap-4">
       <div className="dropdown dropdown-end">
         <div
@@ -76,7 +96,10 @@ export default async function AuthButton() {
           className="btn btn-ghost btn-circle avatar"
         >
           <div className="w-10 rounded-full">
-            <img alt="Tailwind CSS Navbar component" src={profile_url} />
+            <img
+              alt="Tailwind CSS Navbar component"
+              src={myData.profilePicture || "mock_provider.jpeg"}
+            />
           </div>
         </div>
         <ul
@@ -92,14 +115,14 @@ export default async function AuthButton() {
           <li>
             <Link href="/account-settings">Settings</Link>
           </li>
-          {isProvider ? (
+          {myData.isProvider ? (
             <li>
               <Link href="/boardgame-tracking">My boardgames</Link>
             </li>
           ) : null}
 
           <li>
-            <div className="hover:cursor-pointer" onClick={signOutAction}>
+            <div className="hover:cursor-pointer" onClick={handleSignOut}>
               Logout
             </div>
           </li>
@@ -107,13 +130,16 @@ export default async function AuthButton() {
       </div>
     </div>
   ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button>
-    </div>
+    <>
+      <div className="flex gap-2">
+        <Button asChild size="sm" variant={"outline"}>
+          <Link href="/sign-in">Sign in</Link>
+        </Button>
+        <Button asChild size="sm" variant={"default"}>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
+      <button onClick={() => console.log(myData)}>test{myData}</button>
+    </>
   );
 }
