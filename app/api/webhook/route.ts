e3f1ea2921d998ca4cb9cd64e87a86f026cffbc4
MingPV <jeung.pavee@gmail.com>
2025-03-04 20:15:19 +0700

    change stripe apiVersion

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -11,7 +11,7 @@ if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-01-27.acacia",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export async function POST(req: NextRequest) {
