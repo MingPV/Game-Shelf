b85a2f35a2baa17a8e0d72e805cb6ed86bfa80b3
Warintorn Surapolchai <poohmon.bonus@gmail.com>
2025-03-19 00:47:15 +0700

    css form

--- components/user-pages/report-form-card.tsx
+++ components/user-pages/report-form-card.tsx
@@ -12,7 +12,7 @@ export default function ReportFormCard({
 }: {
   userData: UserData;
 }) {
-    const [reportType, setReportType] = useState<string>("general");
+    const [reportType, setReportType] = useState<"general" | "rental">("general");
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
     const [users, setUsers] = useState<UserData[]>([]);
     const [rentalRequests, setRentalRequests] = useState<RentingRequest[]>([]);
@@ -37,6 +37,8 @@ export default function ReportFormCard({
             setRentalRequests(data);
         }
     };
+
+
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -78,146 +80,143 @@ export default function ReportFormCard({
     
     return (
         <div className="flex justify-center items-center w-full pt-4">
-        <div className="card bg-gray-400 bg-opacity-30 w-11/12 md:w-3/5 shadow-2xl">
-            <div className="card-body gap-7">
+        <div className="card gap-2 w-11/12 md:w-3/5">
             <h1 className="flex justify-center text-2xl font-medium">
-                Report Form
+                Report Request Form
             </h1>
+
+            <div className="flex gap-4">
+                <button
+                    type="button"
+                    className={`px-4 py-2 border border-input rounded-lg text-white ${
+                        reportType === "general" ? "bg-gs_gray bg-opacity-30" : "border-gray-400"
+                    }`}
+                    onClick={() => setReportType("general")}
+                >
+                    General
+                </button>
+
+                <button
+                    type="button"
+                    className={`px-6 py-2 border border-input rounded-lg text-white ${
+                        reportType === "rental" ? "bg-gs_gray bg-opacity-30" : "border-gray-400"
+                    }`}
+                    onClick={() => setReportType("rental")}
+                >
+                    Rental
+                </button>
+            </div>
+
+        
+            <div className="card-body border border-input rounded-md gap-7 pl-24 pr-32 py-10 ">
 
             <form
                 className="flex flex-col min-w-64 gap-5 placeholder:text-gs_white"
                 onSubmit={handleSubmit}
             >
 
-                <Label htmlFor="type">Type *</Label>
-                <div className="flex gap-4">
-                    <label className="flex items-center gap-2">
-                        <input
-                            type="radio"
-                            name="type"
-                            value="general"
-                            checked={reportType === "general"}
-                            onChange={() => setReportType("general")}
-                            className="peer hidden"
-                            required
-                        />
-                        <div className="w-5 h-5 border-2 border-gray-400 rounded-full flex items-center justify-center peer-checked:bg-blue-500">
-                        <div className="w-3 h-3 bg-blue-500 rounded-full peer-checked:block hidden"></div>
-                        </div>
-                        General
-                    </label>
-
-                    <label className="flex items-center gap-2">
-                        <input
-                            type="radio"
-                            name="type"
-                            value="rental"
-                            checked={reportType === "rental"}
-                            onChange={() => setReportType("rental")}
-                            className="peer hidden"
-                            required
-                        />
-                        <div className="w-5 h-5 border-2 border-gray-400 rounded-full flex items-center justify-center peer-checked:bg-blue-500">
-                        <div className="w-3 h-3 bg-blue-500 rounded-full peer-checked:block hidden"></div>
-                        </div>
-                        Rental
-                    </label>
+                <div className="grid grid-cols-3 gap-14">
+                    <Label htmlFor="reporter" className="content-center text-md col-span-1">Reporter:</Label>
+                    <Input
+                        className="text-gray-400 col-span-2"
+                        name="reporter"
+                        placeholder="Reporter"
+                        value= {userData?.username}
+                        readOnly
+                        required
+                    />
                 </div>
 
-                <Label htmlFor="reporter">Reporter *</Label>
-                <Input
-                    className="text-gray-300"
-                    name="reporter"
-                    placeholder="Reporter"
-                    value= {userData?.username}
-                    readOnly
-                    required
-                />
-
-                {reportType === "general" ? 
-                    <>
-                        <Label htmlFor="reported">Reported *</Label>
-                        <select
-                            className="w-full rounded-md border border-input px-3 py-2 text-sm bg-transparent"
-                            name="reported"
-                            value={selectedGeneralReportedId}
-                            onChange={(e) => setSelectedGeneralReportedId(e.target.value)}
-                            required
-                        >
-                            {users.map((user) => (
-                                <option key={user.uid} value={user.uid} className="bg-black w-fit rounded-md text-sm">
-                                    {user.username}
-                                </option>
-                            ))}
-                        </select>
-
-                    </> 
-                    :
-                    <>
-                        <Label htmlFor="related_rental">Related Rental *</Label>
-                        <select
-                            className="w-full rounded-md border border-input px-3 py-2 text-sm bg-transparent"
-                            name="related_rental"
-                            value={selectedRentalReportedId}
-                            onChange={(e) => {
-                                setSelectedRentalReportedId(e.target.value)
-                            }}
-                            required
-                        >
-                            {rentalRequests.map((request) => {
-                                return (
-                                    <option 
-                                        key={request.id} 
-                                        value={request.id}
-                                        className="bg-black w-fit rounded-md text-sm"
-                                    >
-                                        {request.id}
+                <div className="grid grid-cols-3 gap-14">
+                    {reportType === "general" ? 
+                        <>
+                            <Label htmlFor="reported" className="content-center text-md col-span-1">Reported User:</Label>
+                            <select
+                                className="w-full rounded-md border border-input px-3 py-2 text-sm bg-transparent col-span-2"
+                                name="reported"
+                                value={selectedGeneralReportedId}
+                                onChange={(e) => setSelectedGeneralReportedId(e.target.value)}
+                                required
+                            >
+                                {users.map((user) => (
+                                    <option key={user.uid} value={user.uid} className="bg-black w-fit rounded-md text-sm">
+                                        {user.username}
                                     </option>
-                                )
-                                }
-                            )}
-                        </select>
-                    </> 
-                }
+                                ))}
+                            </select>
 
-                <Label htmlFor="topic">Topic *</Label>
-                <Input
-                    className="placeholder:text-gray-300"
-                    name="topic"
-                    placeholder="Topic"
-                    required
-                />
+                        </> 
+                        :
+                        <>
+                            <Label htmlFor="related_rental" className="content-center text-md col-span-1">Related Rental:</Label>
+                            <select
+                                className="w-full rounded-md border border-input px-3 py-2 text-sm bg-transparent col-span-2"
+                                name="related_rental"
+                                value={selectedRentalReportedId}
+                                onChange={(e) => {
+                                    setSelectedRentalReportedId(e.target.value)
+                                }}
+                                required
+                            >
+                                {rentalRequests.map((request) => {
+                                    return (
+                                        <option 
+                                            key={request.id} 
+                                            value={request.id}
+                                            className="bg-black w-fit rounded-md text-sm"
+                                        >
+                                            {request.id}
+                                        </option>
+                                    )
+                                    }
+                                )}
+                            </select>
+                        </> 
+                    }
+                </div>
+                
+                <div className="grid grid-cols-3 gap-14">
+                    <Label htmlFor="topic" className="content-center text-md col-span-1">Topic:</Label>
+                    <Input
+                        className="placeholder:text-gray-400 col-span-2"
+                        name="topic"
+                        placeholder="Topic"
+                        required
+                    />
+                </div>
 
-
-                <Label htmlFor="details">Details *</Label>
-                <textarea
-                    className="flex min-h-[100px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm 
-                                ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium 
-                                focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
-                                disabled:cursor-not-allowed disabled:opacity-50 placeholder:text-gray-300"
-                    name="details"
-                    placeholder="Details"
-                    required
-                ></textarea>
-
-                <div className="flex justify-center gap-10">
-                    <button
-                        type="submit"
-                        className="btn rounded-full bg-gs_purple_gradient hover:bg-opacity-60 border-none w-min px-16"
-                    >
-                        Cancel
-                    </button>
-
-                    <button
-                        type="submit"
-                        className="btn rounded-full bg-gs_purple_gradient hover:bg-opacity-60 border-none w-min px-16"
-                    >
-                        {isSubmitting ? "Sending.." : "Submit"}
-                    </button>
+                <div className="grid grid-cols-3 gap-14">
+                    <Label htmlFor="details" className="text-md grid-cols-1">Details:</Label>
+                    <textarea
+                        className="flex min-h-36 w-full rounded-md border border-input bg-background px-3 py-2 text-sm 
+                                    ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium 
+                                    focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+                                    disabled:cursor-not-allowed disabled:opacity-50 placeholder:text-gray-400 col-span-2"
+                        name="details"
+                        placeholder="Details"
+                        required
+                    ></textarea>
                 </div>
 
             </form>
             </div>
+
+            <div className="flex justify-center gap-10 mt-7">
+                <button
+                    type="submit"
+                    className="btn rounded-full text-md bg-gs_gray bg-opacity-50 hover:bg-black hover:bg-opacity-25 border-none w-min px-16"
+                >
+                    Cancel
+                </button>
+
+                <button
+                    type="submit"
+                    className="btn rounded-full text-md bg-gs_purple_gradient hover:bg-opacity-60 border-none w-min px-16"
+                >
+                    {isSubmitting ? "Sending.." : "Submit"}
+                </button>
+            </div>
+
         </div>
         </div>
     );
