ea83c2b596849c8d12321799a2ea8ff3466c008b
Warintorn Surapolchai <poohmon.bonus@gmail.com>
2025-04-07 15:34:10 +0700

    update for execution in swagger

--- utils/swagger.ts
+++ utils/swagger.ts
@@ -10,5 +10,15 @@ export const getApiDocs = async () => createSwaggerSpec({
       version: "1.0.0",
       description: "API documentation for Game Shelf",
     },
+    servers: [
+      {
+        url: "http://localhost:3000", // ✅ สำหรับตอน dev
+        description: "Local dev server",
+      },
+      {
+        url: "https://boardgame-shelf.vercel.app", // ✅ สำหรับ production (เช่น deploy บน Vercel)
+        description: "Production server",
+      },
+    ],
   },
 });
